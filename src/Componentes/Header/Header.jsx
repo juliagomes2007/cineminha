76fe9dfb08f688b2e29755dd2fb0d3a73a1bff4f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-13 13:03:45 -0300

    cineminha

---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar";
+
+
+function Header() {
+    return (  
+        <header>
+            <span>Olá, visitantes</span>
+            <h1>Cineminha po</h1>
+            <Navbar/>
+        </header>
+    );
+}
+
+export default Header;
